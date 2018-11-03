d8c16ce82ac0eb2077eb37180628e63f6c1bab5b
Zihao Xu <zxql2015@mymail.pomona.edu>
2018-11-03 10:21:29 -0700

    Merged

---
+++ website/main.js
@@ -0,0 +1,31 @@
+function onLoad() {
+
+  // Data should be updated with 20 most common names
+
+  for (var key in data.names) {
+    $('#myDropdown').append('<a href="#">'+data.names[key]+'</a>')
+  }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+
+
+}
